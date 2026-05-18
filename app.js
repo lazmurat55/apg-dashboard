44aaa855cc4151bba03a0573ea980d39a05e7c74
lazmurat55 <sem55@yaani.com>
2026-05-18 21:14:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -336,8 +336,8 @@ function removeStaff(name) {
 }
 
 function escapeHtml(str) {
-    if (!str) return '';
-    return str.replace(/[&<>]/g, function(m) {
+    if (str === undefined || str === null) return '';
+    return String(str).replace(/[&<>]/g, function(m) {
         if (m === '&') return '&amp;';
         if (m === '<') return '&lt;';
         if (m === '>') return '&gt;';
